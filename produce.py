4ebd0a7ba111c20cb11a3fd928cc4ffe26224d87
Rob Harrigan <harrigan.rob@gmail.com>
2018-09-01 10:26:59 -0400

    Message delivery output

--- produce.py
+++ produce.py
@@ -10,12 +10,24 @@ options = {
 }
 producer = Producer(**options)
 
+count = 0
+
+def on_delivered(err, msg):
+    global count
+    if err:
+       print err, msg.value()
+    else:
+        count += 1
+
 with open('/usr/share/dict/words') as fh:
     for line in fh:
         word = line.strip()
-        producer.produce('words', word)
-        print word
-        #time.sleep(random.random())
+
+        # produce is asynchronous
+        producer.produce('words', word, on_delivery=on_delivered)
+        time.sleep(random.random())
 
     # need to force a flush so all data is sent
     producer.flush()
+
+print "{} messages delivered".format(count)
